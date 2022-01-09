0e400784c31175e03c51ba45f197d5bdfc5dbc24
Dup4 <lyuzhi.pan@gmail.com>
2022-01-09 18:06:34 +0800

    feat: add template

---
+++ template/t.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+#define Mkp make_pair
+#define All(x) (x).begin(), (x).end()
+using db = double;
+using ll = long long;
+using ull = unsigned long long;
+using pII = pair<int, int>;
+using pLL = pair<ll, ll>;
+// head
