d63a832f8279bbcd8f8e8643a2ba8a623925b1b9
Dup4 <lyuzhi.pan@gmail.com>
2022-01-23 00:32:10 +0800

    feat: update template

--- template/template.hpp
+++ template/template.hpp
@@ -1,21 +1,54 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
-#ifdef HOME
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define lowbit(x) ((x) & (-(x)))
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll mod = 1e9 + 7;
+
+template <typename T, typename S>
+inline bool chmax(T &a, const S &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T &a, const S &b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
 #include <debug.hpp>
 #else
 #define dbg(...)
 #endif
-
-using namespace std;
-#define endl "\n"
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-#define Mkp make_pair
-#define All(x) (x).begin(), (x).end()
-using db = double;
-using ll = long long;
-using ull = unsigned long long;
-using pII = pair<int, int>;
-using pLL = pair<ll, ll>;
 // head
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
