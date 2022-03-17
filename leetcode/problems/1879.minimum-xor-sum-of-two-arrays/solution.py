fea65db6c74b38fb11e048a2b35e2aacbc248609
Dup4 <lyuzhi.pan@gmail.com>
2022-03-18 00:31:25 +0800

    feat: improve prov 1879

--- leetcode/problems/1879.minimum-xor-sum-of-two-arrays/solution.py
+++ leetcode/problems/1879.minimum-xor-sum-of-two-arrays/solution.py
@@ -4,21 +4,14 @@ from typing import List
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
-        f = [[0x3f3f3f3f for i in range(1 << n)] for i in range(n + 1)]
-        f[0][0] = 0
-        for i in range(n):
-            for S in range(1 << n):
-                cnt = 0
+        f = [0x3f3f3f3f for i in range(1 << n)]
+        f[0] = 0
+        for S in range(1, 1 << n):
+            cnt = bin(S).count("1")
 
-                for j in range(n):
-                    if (S >> j) & 1:
-                        cnt += 1
+            for i in range(n):
+                if (S >> i) & 1:
+                    f[S] = min(f[S], f[S ^ (1 << i)] +
+                               (nums1[cnt - 1] ^ nums2[i]))
 
-                if cnt == i:
-                    for j in range(n):
-                        if ((S >> j) & 1) == 0:
-                            SS = S | (1 << j)
-                            f[i + 1][SS] = min(f[i + 1][SS],
-                                               f[i][S] + (nums1[i] ^ nums2[j]))
-
-        return min(f[n])
+        return f[-1]
