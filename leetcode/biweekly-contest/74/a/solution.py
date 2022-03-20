5c11849f18b7346741b6018f025f870726dc0ee6
Dup4 <lyuzhi.pan@gmail.com>
2022-03-20 09:14:18 +0800

    feat: improve biweekly 74 a

--- leetcode/biweekly-contest/74/a/solution.py
+++ leetcode/biweekly-contest/74/a/solution.py
@@ -4,8 +4,4 @@ from typing import Counter, List
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         a = Counter(nums)
-        n = len(nums)
-        for k in a:
-            if a[k] & 1:
-                return False
-        return (n // 2) >= len(a.keys())
+        return all((v & 1 == 0) for v in a.values()) and (len(nums) // 2) >= len(a.keys())
