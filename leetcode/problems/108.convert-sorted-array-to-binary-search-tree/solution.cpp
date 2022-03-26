46f3dc7b8f4719abbafd52716517ae07430d1680
Dup4 <lyuzhi.pan@gmail.com>
2022-03-26 20:44:46 +0800

    feat: p 108

---
+++ leetcode/problems/108.convert-sorted-array-to-binary-search-tree/solution.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll mod = 1e9 + 7;
+
+template <typename T, typename S>
+inline bool chmax(T &a, const S &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T &a, const S &b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#ifdef LOCAL
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif
+
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums) {
+        int n = nums.size();
+        auto rt = new TreeNode();
+
+        auto dfs = [&](auto self, int l, int r, TreeNode *rt) -> void {
+            int mid = (l + r) >> 1;
+            rt->val = nums[mid];
+            if (l < mid) {
+                rt->left = new TreeNode();
+                self(self, l, mid - 1, rt->left);
+            }
+
+            if (mid < r) {
+                rt->right = new TreeNode();
+                self(self, mid + 1, r, rt->right);
+            }
+        };
+
+        dfs(dfs, 0, n - 1, rt);
+        return rt;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
