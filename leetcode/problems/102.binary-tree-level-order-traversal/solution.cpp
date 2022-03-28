92ba2d87a0076227ea3b7a633a183a604dc001b8
Dup4 <lyuzhi.pan@gmail.com>
2022-03-28 23:50:53 +0800

    feat: p 102

---
+++ leetcode/problems/102.binary-tree-level-order-traversal/solution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll mod = 1e9 + 7;
+
+template <typename T, typename S>
+inline bool chmax(T &a, const S &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T &a, const S &b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+#ifdef LOCAL
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    void dfs(TreeNode *rt, int dep) {
+        if (!rt) {
+            return;
+        }
+
+        if (res.size() < dep + 1) {
+            res.push_back(vector<int>());
+        }
+
+        res[dep].push_back(rt->val);
+        dfs(rt->left, dep + 1);
+        dfs(rt->right, dep + 1);
+    }
+
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        dfs(root, 0);
+        return res;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
