a3ad73f13fd90105da71102c0d67a970e1b4912c
Dup4 <lyuzhi.pan@gmail.com>
2022-03-29 21:13:04 +0800

    feat: p 905

---
+++ leetcode/problems/905.sort-array-by-parity/solution.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        return list(filter(lambda x: x % 2 == 0, nums)) + list(filter(lambda x: x % 2 == 1, nums))
