1fce2b69d1644a67bbc9922afb6b56afc075d5a4
Dup4 <lyuzhi.pan@gmail.com>
2022-03-31 00:11:12 +0800

    fix: p 44

--- leetcode/problems/44.wildcard-matching/solution.js
+++ leetcode/problems/44.wildcard-matching/solution.js
@@ -5,7 +5,7 @@
  */
 var isMatch = function (s, p) {
     p = p.replace(/\?/g, ".");
-    p = p.replace(/\*/g, ".*");
+    p = p.replace(/\*+/g, ".*");
     const r = new RegExp(`^${p}$`);
     return r.test(s);
 };
