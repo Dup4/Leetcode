b9aaf8cd9be415e33b24011c6bc01e6a110606f9
Dup4 <lyuzhi.pan@gmail.com>
2022-03-30 22:39:26 +0800

    feat: p 509

---
+++ leetcode/problems/509.fibonacci-number/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        f = [0 for i in range(n + 2)]
+        f[1] = 1
+        for i in range(2, n + 1):
+            f[i] = f[i - 1] + f[i - 2]
+        return f[n]
