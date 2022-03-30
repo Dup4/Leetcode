75d5162d892633eadd92d2e1e90693719e6c8782
Dup4 <lyuzhi.pan@gmail.com>
2022-03-31 00:03:15 +0800

    fix: p 520

--- leetcode/problems/520.detect-capital/solution.py
+++ leetcode/problems/520.detect-capital/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return word.isupper() or word.islower() or all(word[i].isupper() ^ (i & 1) for i in range(len(word)))
+        return word.isupper() or word.islower() or all(word[i].isupper() ^ (i > 0) for i in range(len(word)))
