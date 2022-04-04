c025be6a2ad290342e0278a313a7e22d2c67a56d
Dup4 <lyuzhi.pan@gmail.com>
2022-04-04 22:48:47 +0800

    feat: p 448

---
+++ leetcode/problems/448.find-all-numbers-disappeared-in-an-array/solution.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll mod = 1e9 + 7;
+
+template <typename T, typename S>
+inline bool chmax(T &a, const S &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T &a, const S &b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int x = abs(nums[i]);
+            if (nums[x - 1] > 0) {
+                nums[x - 1] *= -1;
+            }
+        }
+
+        auto res = vector<int>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                res.push_back(i + 1);
+            }
+        }
+
+        return res;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
