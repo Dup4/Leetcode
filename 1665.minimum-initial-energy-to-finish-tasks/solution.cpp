968f298f72f854a66f8854d2cd9db78d3df29f24
Dup4 <lyuzhi.pan@gmail.com>
2022-04-07 21:31:01 +0800

    feat: p 1665

---
+++ 1665.minimum-initial-energy-to-finish-tasks/solution.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll mod = 1e9 + 7;
+
+template <typename T, typename S>
+inline bool chmax(T& a, const S& b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T& a, const S& b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(all(tasks), [&](const vector<int>& a, const vector<int>& b) {
+            return a[1] - a[0] > b[1] - b[0];
+        });
+
+        int res = 0;
+        int cur = 0;
+        for (const auto& t : tasks) {
+            if (cur < t[1]) {
+                res += t[1] - cur;
+                cur = t[1];
+            }
+
+            cur -= t[0];
+        }
+
+        return res;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
