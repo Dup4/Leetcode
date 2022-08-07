c09acee4155b1b1056e928447ca2cff040f5a138
Dup4 <lyuzhi.pan@gmail.com>
2022-08-07 09:42:03 +0800

    feat: biweekly 84

---
+++ leetcode/biweekly-contest/84/a/solution.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename S>
+inline bool chmax(T& a, const S& b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T& a, const S& b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        auto mp = map<int, int>();
+        for (const auto& it : items1) {
+            mp[it[0]] += it[1];
+        }
+
+        for (const auto& it : items2) {
+            mp[it[0]] += it[1];
+        }
+
+        vector<vector<int>> res;
+        res.reserve(mp.size());
+        for (const auto& it : mp) {
+            res.emplace_back(vector<int>({it.first, it.second}));
+        }
+        return res;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
