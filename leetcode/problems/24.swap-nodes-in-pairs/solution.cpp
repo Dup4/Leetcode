2c690598965d9428277755bfa5660a856295b647
Dup4 <lyuzhi.pan@gmail.com>
2023-10-11 09:04:42 +0800

    feat: swap-nodes-in-pairs
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ leetcode/problems/24.swap-nodes-in-pairs/solution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define rall rbegin(a), rend(a)
+#define bitcnt(x) (__builtin_popcountll(x))
+#define complete_unique(a) a.erase(unique(begin(a), end(a)), end(a))
+#define mst(x, a) memset(x, a, sizeof(x))
+#define MP make_pair
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using VLL = std::vector<ll>;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
+using PLL = std::pair<ll, ll>;
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename S>
+inline bool chmax(T &a, const S &b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+template <typename T, typename S>
+inline bool chmin(T &a, const S &b) {
+    return a > b ? a = b, 1 : 0;
+}
+
+#ifdef LOCAL
+#include <debug.hpp>
+#else
+#define dbg(...)
+#endif
+// head
+
+#ifdef LOCAL
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#endif
+
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        ListNode *pre = nullptr;
+        auto *tmp = head;
+        int ix = 0;
+        while (tmp) {
+            if ((ix & 1) && pre) {
+                swap(pre->val, tmp->val);
+            }
+
+            pre = tmp;
+            tmp = tmp->next;
+
+            ++ix;
+        }
+
+        return head;
+    }
+};
+
+#ifdef LOCAL
+
+int main() {
+    return 0;
+}
+
+#endif
